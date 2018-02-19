a1f384b6c6481023deda5050b0a3df94a1e722c5
Kristian Stamland <krsta@stud.ntnu.no>
2018-02-19 13:26:33 +0100

    Created new branch.

--- src/containers/Setlist.js
+++ src/containers/Setlist.js
@@ -7,14 +7,36 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
+import firebase from 'firebase';
+
+// Creating Action creators.
+const fetchBandArrangments = (bandId) => async dispatch => {
+    let snapshot =  await firebase.firestore().collection(`bands/${bandId}/arrangements`).get();
+    let recieved_arrangements = snapshot.docs.map(async doc => {
+        const arrDoc = await doc.data.ref.get();
+        return {id: arrDoc.id, ...arrDoc.data()};
+    });
+
+    return {type:"BAND_ARRANGEMENTS_FETCH_RESPONSE", arrangements: recieved_arrangements};
+}
+
+
 const styles = {
     root: {
     }
 };
 
 class Setlist extends Component {
+
+    requestBandArrangements(){
+        let bandId = this.props.pathname.split('/')[2];
+        dispatch(fetchBandArrangments(bandId));
+    }
+
+
     componentWillMount() {
         console.log(this.props);
+        this.requestBandArrangements();
     }
 
     render() {
@@ -30,11 +52,14 @@ class Setlist extends Component {
                     </Toolbar>
                 </AppBar>
                 <div>
+
                 </div>
             </div>
         );
     }
 }
+
+
 
 
 export default compose(connect(state => ({
