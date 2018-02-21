8111d0962ecf2c2086c336f43e66c1289893301e
Kristian Stamland <krsta@stud.ntnu.no>
2018-02-21 17:53:22 +0100

    Added Dialog for creating setlist Added a date picker component

--- src/containers/Setlist.js
+++ src/containers/Setlist.js
@@ -7,6 +7,12 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
+import {
+    Button, Card, CardContent, CardMedia, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, Menu, MenuItem,
+    Snackbar,
+    TextField
+} from "material-ui";
+
 import firebase from 'firebase';
 
 // Creating Action creators.
@@ -16,9 +22,13 @@ const fetchBandArrangments = (bandId) => async dispatch => {
         const arrDoc = await doc.data.ref.get();
         return {id: arrDoc.id, ...arrDoc.data()};
     });
+    
+    console.log(recieved_arrangements);
 
-    return {type:"BAND_ARRANGEMENTS_FETCH_RESPONSE", arrangements: recieved_arrangements};
+    //return {type:"BAND_ARRANGEMENTS_FETCH_RESPONSE", arrangements: recieved_arrangements};
 }
+
+
 
 
 const styles = {
@@ -30,7 +40,8 @@ class Setlist extends Component {
 
     requestBandArrangements(){
         let bandId = this.props.pathname.split('/')[2];
-        dispatch(fetchBandArrangments(bandId));
+        //dispatch(fetchBandArrangments(bandId));
+        fetchBandArrangments(bandId);
     }
 
 
@@ -38,7 +49,9 @@ class Setlist extends Component {
         console.log(this.props);
         this.requestBandArrangements();
     }
-
+    _onDialogClose(type) {
+        this.setState({[`${type}DialogOpen`]: false});
+    }
     render() {
         const {classes} = this.props;
 
@@ -54,6 +67,7 @@ class Setlist extends Component {
                 <div>
 
                 </div>
+
             </div>
         );
     }
@@ -64,4 +78,35 @@ class Setlist extends Component {
 
 export default compose(connect(state => ({
     user: state.default.user,
+    band:state.default.band,
+    pathname: state.router.location.pathname
 })), withStyles(styles))(Setlist);
+
+{/* <Dialog open={addArrDialogOpen} onClose={() => this._onDialogClose('create')}>
+<DialogTitle>Add Arrangment</DialogTitle>
+<DialogContent>
+    <TextField
+        label="Name"
+        margin="normal"
+        onChange={e => this.setState({bandName: e.target.value})}
+    />
+</DialogContent>
+<DialogActions>
+    <Button color="primary" onClick={() => this._onDialogClose('create')}>Cancel</Button>
+    <Button color="primary" onClick={() => this._onDialogSubmit('create')} autoFocus>Create</Button>
+</DialogActions>
+</Dialog>
+<Dialog open={joinDialogOpen} onClose={() => this._onDialogClose('join')}>
+<DialogTitle>Join Band</DialogTitle>
+<DialogContent>
+    <TextField
+        label="Code"
+        margin="normal"
+        onChange={e => this.setState({bandCode: e.target.value})}
+    />
+</DialogContent>
+<DialogActions>
+    <Button color="primary" onClick={() => this._onDialogClose('join')}>Cancel</Button>
+    <Button color="primary" onClick={() => this._onDialogSubmit('join')} autoFocus>Join</Button>
+</DialogActions>
+</Dialog> */}
