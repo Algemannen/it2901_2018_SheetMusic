7bb4a45b70f5d7a16f4d6a9fadf4d24fad591f90
Kristian Stamland <kristiansta@hotmail.com>
2018-04-20 13:06:08 +0200

    Setting up branch

---
+++ hosting/src/components/dialogs/DownloadSetlistDialog.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {TextField, withStyles} from "material-ui";
+import {DatePicker} from "material-ui-pickers"
+
+import AsyncDialog from "./AsyncDialog";
+
+const styles = {
+};
+
+class CreateSetlistDialog extends React.Component {
+    state = {
+        title: '',
+        date: new Date(),
+    };
+
+    componentDidMount() {
+        this.props.onRef(this);
+    }
+
+    componentWillUnmount() {
+        this.props.onRef(undefined)
+    }
+
+    async open() {
+        await this.dialog.open();
+        return {title: this.state.title, date: this.state.date};
+    }
+
+    _onTitleInputChange = e => {
+        this.setState({title: e.target.value});
+    };
+
+    _onDateChange = date => {
+        this.setState({date: date});
+    };
+
+    render() {
+        const {classes} = this.props;
+        const {title, date} = this.state;
+
+        return <AsyncDialog title='Create Setlist' confirmText='Create' onRef={ref => this.dialog = ref}>
+            <TextField label='Title' onChange={this._onTitleInputChange} style={{marginBottom: 20}}/>
+            <DatePicker
+                value={date}
+                onChange={this._onDateChange}
+            />
+        </AsyncDialog>
+    }
+}
+
+export default withStyles(styles)(CreateSetlistDialog);
